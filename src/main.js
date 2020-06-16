777f176cfd85263cbdd45cee7b235098367c777d
sullay <1181518458@qq.com>
2020-06-16 10:37:31 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import './plugins/axios'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import './assets/css/base.css'
+import GlobalConfig from './config'
+import './plugins/element.js'
+
+Vue.use(GlobalConfig)
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
